7c30039186c1f7d772bc8cc9011ec6f9bfec5c4b
NegaTiv1298 <levkristi@i.ua>
2021-02-11 23:10:52 +0200

    Ex.6.13

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 $a = 30;
-$b = [1, 2, 3, 5, 6, 10, 15, 30];
 $arr = [];
-foreach ($b as $elem) {
-        $arr[] = ($a%$elem==0);
-    }
+for ($i = 1; $i <= $a; $i++) {
+   if ($a % $i == 0) {
+       $arr[] = $i;
+   }
+}
 var_dump($arr);
