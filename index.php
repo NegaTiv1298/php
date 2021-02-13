afb93132560e7a6ddc8fef825a3b2eb44402351a
NegaTiv1298 <levkristi@i.ua>
2021-02-13 16:57:47 +0200

    Ex.7.9

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
-$password = '12345';
-if (strlen($password) >= 5 & strlen($password) < 10) echo 'Пароль подходит'; else echo 'Придумайте другой пароль';
+$a = 'html css php';
+echo substr($a, 0, 4);
+echo substr($a, 4, 3);
+echo substr($a, 7);
