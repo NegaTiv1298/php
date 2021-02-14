313b2fa491ffdc4343721132a288a9193892f2b8
NegaTiv1298 <levkristi@i.ua>
2021-02-14 23:17:35 +0200

    Ex.7.52

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
-$str = ord('c');
-if ($str >= 65 && $str <=90) {
-    echo $str. ' - Большая буква';
-} else {
-    echo $str. ' - Маленькая буква';
-}
+$str = 'ab-cd-ef';
+echo strchr($str, '-');
