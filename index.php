42a380a6e07f0bd86dce869b414408014c09c1fe
NegaTiv1298 <levkristi@i.ua>
2021-02-16 23:14:44 +0200

    Ex.8.17

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
 $arr = [1, 2, 3, 4, 5];
-$result = array_splice($arr, 3, 0, ['a','b','c']);
+$result = array_splice($arr, 1, 0, ['a','b'] );
+$result2 = array_splice($arr, 6, 0, 'c');
+$result3 = array_splice($arr, 8, 0, 'e');
 var_dump($arr);
