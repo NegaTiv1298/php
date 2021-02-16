0781f5921aec1d9175949c7aa2366ecc0af1b2b6
NegaTiv1298 <levkristi@i.ua>
2021-02-16 22:14:25 +0200

    Ex.8.1

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
-$arr = [3, 11, 23, 42, 16, 37];
-foreach ($arr as $elem) {
-    if (strpos($elem, '3') !== false) {
-        echo $elem. '<br>';
-    }
-}
+$arr = [2, 3, 5, 7];
+echo count($arr);
