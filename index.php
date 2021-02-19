4c7aec60be080aa842491f8f1e12e27c0c2c5f25
NegaTiv1298 <levkristi@i.ua>
2021-02-19 23:52:04 +0200

    Ex.10.2

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
+<form action="" method="get">
+    <input placeholder="Введите имя" type="text" name="name">
+    <input placeholder="Введите возраст" type="text" name="age">
+    <br><br>
+    <textarea placeholder="Введите сообщение" name="text"></textarea>
+    <input type="submit" name="sumbit">
+</form>
 <?php
-$str = '1234567890';
-$arr = str_split($str, 2);
-var_dump(array_sum($arr));
+if (isset($_REQUEST['sumbit'])) {
+    $name = strip_tags($_REQUEST['name']);
+    $age = strip_tags($_REQUEST['age']);
+    $text = strip_tags($_REQUEST ['text']);
+    echo 'Привет, '. $name. ', '. $age. '<br>';
+    echo 'Твое сообщение:'. $text;
+}
