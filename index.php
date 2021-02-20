6f7e366a66303a2ff6cff97f39196f7a6e31da02
NegaTiv1298 <levkristi@i.ua>
2021-02-20 17:30:37 +0200

    Ex.11.1

--- index.php
+++ index.php
@@ -1,14 +1,2 @@
-<form action="" method="GET">
-    <input name="name" value="<?php if (isset($_GET['name'])) echo $_GET['name']; ?>">
-    <textarea placeholder="Введите сообщение" name="text">
-        <?php if (isset($_GET['text'])) echo $_GET['text']; ?>
-    </textarea>
-    <input type="submit">
-</form>
 <?php
-if (isset($_REQUEST['submit'])) {
-    $name = strip_tags($_REQUEST['name']);
-    $text = strip_tags($_REQUEST['text']);
-    echo $name;
-}
-?>
+echo time();
