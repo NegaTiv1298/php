6feef320bc88ac9169e99d5cfc152891907d87df
NegaTiv1298 <levkristi@i.ua>
2021-02-21 00:00:30 +0200

    Ex.11.9

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
-echo date('d.m.Y', mktime(0,0,0,2,12,2025));
+$week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+$day = date('w');
+echo $week[$day];
+$day1 = date('w', mktime(0,0,0,6,6,2006));
+echo $week[$day1];
+// Не поняв умову про день народження. Вивів просто день який був в 2006 році.
