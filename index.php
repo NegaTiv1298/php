4a7d24234c631a5eb570019db7c6006eb17025d1
NegaTiv1298 <levkristi@i.ua>
2021-02-21 00:31:15 +0200

    Ex.11.12

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
+<form action="" method="get">
+    <input type="text" name="year">
+    <input type="submit" name="sumbit">
+</form>
 <?php
-echo date('t');
+if (isset($_REQUEST['year'])) {
+    $year = trim(strip_tags($_REQUEST['year']));
+    if (strlen($year) == 4) {
+        if (date('L', mktime(0,0,0,1,1, $year ))) {
+            echo 'Высокосный';
+        } else {
+            echo 'Не высокосный';
+        }
+    } else {
+        echo 'Ошибка, Введите год(4 цифры)';
+    }
+}
