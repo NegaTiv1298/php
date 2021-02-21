20700fc91693b2cf341713b177cd6dcb286b6aae
NegaTiv1298 <levkristi@i.ua>
2021-02-21 22:49:20 +0200

    Ex.11.15

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <form action="" method="get">
-    <input type="text" placeholder="Пример(2025-12-31)" name="date">
+    <input type="text" placeholder="Пример(2025-12-31)" name="date1">
+    <input type="text" placeholder="Пример(2025-12-31)" name="date2">
     <input type="submit" name="sumbit">
 </form>
 <?php
-if (isset($_REQUEST['date'])) {
-    $date = trim(strip_tags($_REQUEST['date']));
-    $arr = explode('-', $date);
-    $mtime = mktime(0,0,0, $arr[1], $arr[2], $arr[0]);
-    $month = [1 => 'Янв', 'Фев', 'Март', 'Апр', 'Май', 'Июнь', 'Июль', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'];
-    $day = date('n', $mtime);
-    echo $month[$day];
+if (isset($_REQUEST['date1']) && $_REQUEST['date2']) {
+    $date1 = trim(strip_tags($_REQUEST['date1']));
+    $date2 = trim(strip_tags($_REQUEST['date2']));
+    if ($date1 > $date2) {
+        echo $date1;
+    } else {
+        echo $date2;
+    }
 }
