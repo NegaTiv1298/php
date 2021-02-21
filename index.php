2d712c9039dab56777166591b9996e1bc693e944
NegaTiv1298 <levkristi@i.ua>
2021-02-21 22:53:32 +0200

    Ex.11.16

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
-<form action="" method="get">
-    <input type="text" placeholder="Пример(2025-12-31)" name="date1">
-    <input type="text" placeholder="Пример(2025-12-31)" name="date2">
-    <input type="submit" name="sumbit">
-</form>
 <?php
-if (isset($_REQUEST['date1']) && $_REQUEST['date2']) {
-    $date1 = trim(strip_tags($_REQUEST['date1']));
-    $date2 = trim(strip_tags($_REQUEST['date2']));
-    if ($date1 > $date2) {
-        echo $date1;
-    } else {
-        echo $date2;
-    }
-}
+$timestamp = strtotime('2025-12-31');
+echo date('d-m-Y', $timestamp);
