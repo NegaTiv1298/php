ffb2221235c5bac780d52767d48dd75e53c2f8d1
NegaTiv1298 <levkristi@i.ua>
2021-02-21 22:40:05 +0200

    Ex.11.14

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <form action="" method="get">
-    <input type="text" placeholder="Пример(21.12.2000)" name="date">
+    <input type="text" placeholder="Пример(2025-12-31)" name="date">
     <input type="submit" name="sumbit">
 </form>
 <?php
 if (isset($_REQUEST['date'])) {
     $date = trim(strip_tags($_REQUEST['date']));
-    $arr = explode('.', $date);
-    $mtime = mktime(0,0,0, $arr[1], $arr[0], $arr[2]);
-    $week = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
-    $day = date('w', $mtime);
-    echo $week[$day];
+    $arr = explode('-', $date);
+    $mtime = mktime(0,0,0, $arr[1], $arr[2], $arr[0]);
+    $month = [1 => 'Янв', 'Фев', 'Март', 'Апр', 'Май', 'Июнь', 'Июль', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'];
+    $day = date('n', $mtime);
+    echo $month[$day];
 }
