1a65354e5e2e6e8d1446f7f9f3740b511c3f00a7
NegaTiv1298 <levkristi@i.ua>
2021-02-22 23:17:05 +0200

    Ex.11.21

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
-<form action="" method="get">
-    <input type="text" name="year" placeholder="Введите год">
-    <input type="submit" name="sumbit">
-</form>
 <?php
-if (isset($_REQUEST['year'])) {
-    $year = $_REQUEST['year'];
-    for ($i = 1; $i <= 12; $i++) {
-        if (date('w', mktime(0, 0, 0, $i, 13, $year)) == 5) {
-            $friday13[] = date('d-m-Y', mktime(0, 0, 0, $i, 13, $year));
-        }
-    }
-}
-// По суті списав рішення задачі. Поняв всьо крім ( if (date..... )) == 5 ) Шо то за "== 5" я хз.
+$week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+$date = date_create('2021-02-22');
+$date = date_modify($date, '-100 day');
+$day = date_format($date, 'w');
+echo $day. '<br>';
+echo $week[$day];
