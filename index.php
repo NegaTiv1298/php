0dbb437a2ff78b0b620bd1d331adcb7ea5b7737c
NegaTiv1298 <levkristi@i.ua>
2021-02-23 22:45:48 +0200

    Ex.12.1

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-$week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
-$date = date_create('2021-02-22');
-$date = date_modify($date, '-100 day');
-$day = date_format($date, 'w');
-echo $day. '<br>';
-echo $week[$day];
+function kv($num)
+{
+    return $num * $num;
+}
