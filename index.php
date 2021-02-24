fbf83b3238114cf4d3c9670a0db1da9c201547ab
NegaTiv1298 <levkristi@i.ua>
2021-02-24 22:47:43 +0200

    Ex.16.5

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
-$arr = range(1,10);
+$arr = [[1, 2, 3], [4, 5], [6]];
 $sum = 0;
-$kol = 0;
-for ($i = 0; $i < count($arr); $i++) {
-    $sum += $arr[$i];
-    $kol++;
-    if ($sum > 10) {
-        break;
+foreach ($arr as $elem) {
+    foreach ($elem as $subElem) {
+        $sum += $subElem;
     }
 }
-echo $kol;
+echo $sum;
