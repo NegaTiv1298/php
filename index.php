63ccc0678fb93d5dc43093f3ee7d15f8ec985b0f
NegaTiv1298 <levkristi@i.ua>
2021-02-24 15:40:02 +0200

    Ex.14.1

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
-$num = [1, 2, 3, 4, 5, 6];
-$flag = false;
-$kol = count($num);
-for ($i = 1; $i <= $kol; $i++) {
-    if ($num[$i] == $num[$i - 1]) {
-        $flag = true;
-        break;
-    }
-}
-if ($flag) {
-    echo 'True';
-} else {
-    echo 'False';
+function ab($a, $b)
+{
+    return $a == $b;
 }
