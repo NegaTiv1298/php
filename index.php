483c2ac24a4fabe5c4d9c63d1aefb1d46cf2defa
NegaTiv1298 <levkristi@i.ua>
2021-02-24 16:45:12 +0200

    Ex.16.2

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 $arr = [];
-$str = '';
-for ($i = 1; $i <= 10; $i++) {
-    $str .= 'x';
+
+for ($i = 1; $i <= 9; $i++) {
+    $str = '';
+    for ($j = 1; $j <= $i; $j++) {
+        $str .= $i;
+    }
+
     $arr[] = $str;
 }
 var_dump($arr);
