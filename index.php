c1a0de04a61396d9f2708f6ff137903b21fc014d
NegaTiv1298 <levkristi@i.ua>
2021-02-24 22:49:31 +0200

    Ex.16.6

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
-$arr = [[1, 2, 3], [4, 5], [6]];
+$arr = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
 $sum = 0;
 foreach ($arr as $elem) {
     foreach ($elem as $subElem) {
-        $sum += $subElem;
+        foreach ($subElem as $subsubElem) {
+            $sum += $subsubElem;
+        }
     }
 }
 echo $sum;
