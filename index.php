34d9d23730f14d869461651e282809c8686bc07a
NegaTiv1298 <levkristi@i.ua>
2021-02-28 22:06:07 +0200

    Ex.19.1

--- index.php
+++ index.php
@@ -1,31 +1,5 @@
 <?php
-function getDivisors($num)
+function cut($str, $num = 10)
 {
-    $arr = [];
-    for ($i = 1; $i <= $num; $i++) {
-        if ($num % $i == 0) {
-            $arr[] = $num;
-        }
-    }
-    return $arr;
+    return substr($str, 0 , $num);
 }
-function ArraySum($arr)
-{
-    $sum = 0;
-    foreach ($arr as $elem) {
-        $sum += $elem;
-    }
-    return $sum;
-} // От сюда я не зміг сам зробити.
-$friend = [];
-for ($i = 1; $i < 1000; $i++) {
-    $sum1 = ArraySum(getDivisors($i));
-    if ($sum1 > 1) {
-        for ($j = $i + 1; $j <= 1000; $j++) {
-            if ($sum1 == ArraySum(getDivisors($j))) {
-                $friend[$i][] = $j;
-            }
-        }
-    }
-}
-var_dump($friend);
