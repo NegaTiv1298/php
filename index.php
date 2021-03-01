79189a30a3310a6f2ebb4179dca87f413e9be33a
NegaTiv1298 <levkristi@i.ua>
2021-03-01 22:59:42 +0200

    Ex.20.8

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 function input($type, $name, $value)
 {
-    return '<input type = "'.$type.'" name = "'.$name.'" value = "'.$value.'">';
+    if (isset($_REQUEST[$name])) {
+        $value = $_REQUEST[$name];
+    }
+    return '<input type = "text" name = "'.$name.'" value = "'.$value.' ">';
 }
 echo input('text', 'input', '1');
