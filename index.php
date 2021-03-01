be69723a23f273dd280119bdcd2b98636f5400a4
NegaTiv1298 <levkristi@i.ua>
2021-03-01 22:26:30 +0200

    Ex.20.3

--- index.php
+++ index.php
@@ -1,25 +1,13 @@
 <form action="" method="get">
-    <p>Введите языки которые вы знаете <b>( html, css, php, javascript )</b></p>
-    <input type="text" name="name">
-    <br> <br>
+    <p><b>Знаете ли вы язык 'PHP'?</b></p>
+    <p>Да<input type="radio" name="rad1" value="1"></p>
+    <p>Нет<input type="radio" name="rad2" value="0"></p>
     <input type="submit" name="sumbit">
 </form>
 <?php
-$name = $_REQUEST['name'];
-if (isset($_REQUEST['name'])) {
-    echo $name;
-    echo '<br>';
-} // < Так зробив ізначально я.
-?>
-
-<form action="" method="get">
-    <p>html<input type="checkbox" name="lang[]" value="html"></p>
-    <p>css<input type="checkbox" name="lang[]" value="css"></p>
-    <p>php<input type="checkbox" name="lang[]" value="php"></p>
-    <p>javascript<input type="checkbox" name="lang[]" value="javascript"></p>
-    <input type="submit" name="sumbit">
-</form>
-<?php
-if (isset($_REQUEST ['lang'])) {
-    echo 'Вы знаете'. ' : '. implode(', ', $_REQUEST['lang']);
-} // А так показало в рішенню задачі. Но суть поняв.
+if (isset($_REQUEST ['rad1'])) {
+    echo 'Вы знаете PHP';
+}
+if (isset($_REQUEST ['rad2'])) {
+    echo 'Вы не знаете PHP';
+}
