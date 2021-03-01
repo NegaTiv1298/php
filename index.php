5d7a64c4b2c586e0bffaead734312929ba4af156
NegaTiv1298 <levkristi@i.ua>
2021-03-01 22:16:27 +0200

    Ex.20.2

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
-<form>
-    <input type="text" name="name" placeholder="Введите имя">
-    <input type="checkbox" name="check"> <br> <br>
+<form action="" method="get">
+    <p>Введите языки которые вы знаете <b>( html, css, php, javascript )</b></p>
+    <input type="text" name="name">
+    <br> <br>
     <input type="submit" name="sumbit">
 </form>
 <?php
 $name = $_REQUEST['name'];
-if (isset($_REQUEST ['name']) && isset($_REQUEST ['check']) == 1) {
-    echo 'Привет '. $name;
-}
-if (isset($_REQUEST ['check']) == 0) {
-    echo 'Прощайте '. $name;
-}
+if (isset($_REQUEST['name'])) {
+    echo $name;
+    echo '<br>';
+} // < Так зробив ізначально я.
+?>
+
+<form action="" method="get">
+    <p>html<input type="checkbox" name="lang[]" value="html"></p>
+    <p>css<input type="checkbox" name="lang[]" value="css"></p>
+    <p>php<input type="checkbox" name="lang[]" value="php"></p>
+    <p>javascript<input type="checkbox" name="lang[]" value="javascript"></p>
+    <input type="submit" name="sumbit">
+</form>
+<?php
+if (isset($_REQUEST ['lang'])) {
+    echo 'Вы знаете'. ' : '. implode(', ', $_REQUEST['lang']);
+} // А так показало в рішенню задачі. Но суть поняв.
