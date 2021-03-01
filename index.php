4b8fec8249ec0b0c96d6b0675150c617f841df45
NegaTiv1298 <levkristi@i.ua>
2021-03-01 23:25:24 +0200

    Ex.20.10

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 function input($name)
 {
-    return '<input type="hidden" name="'.$name.'" value="0">
-    <input type="checkbox" name="'.$name.'" value="1">';
+    if (isset($_REQUEST [$name]) && $_REQUEST[$name] == 1) {
+        echo $value = 'checked';
+    } else {
+        echo $value = '';
+    }
+    return '<input type="hidden" name "'.$name.'" value="0">
+    <input type="checkbox" name="'.$name.'" value="1" '.$value.'>';
 }
-echo input('checkbox ');
+echo input('checkbox');
