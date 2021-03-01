c942b34b0725e517119d6fb0b3016c7523f533e1
NegaTiv1298 <levkristi@i.ua>
2021-03-01 22:48:07 +0200

    Ex.20.7

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
-<form action="" method="get">
-    <p><b>Отметьте какие языки вы знаете.</b></p>
-    <select multiple name="name[]">
-        <option>Html</option>
-        <option>Css</option>
-        <option>Php</option>
-        <option>JavaScript</option>
-    </select> <br> <br>
-    <input type="submit" name="sumbit">
-</form>
-
 <?php
-if (isset($_REQUEST ['name'])) {
-    echo 'Вы знате : '. implode(', ', $_REQUEST['name']);
+function input($type, $name, $value)
+{
+    return '<input type = "'.$type.'" name = "'.$name.'" value = "'.$value.'">';
 }
+echo input('text', 'input', '1');
