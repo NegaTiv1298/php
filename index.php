a41fd03c3add31438680df9ad497dcbf22921d15
NegaTiv1298 <levkristi@i.ua>
2021-03-02 22:54:14 +0200

    Ex.21.18

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
-for ($i = 1; $i <= 9; $i++) {
-    for ($j = 1; $j <= $i; $j++) {
-        $str .= $i;
-    }
+$arr = [
+    0=>['name'=>'Коля', 'salary'=>300],
+    1=>['name'=>'Вася', 'salary'=>400],
+    2=>['name'=>'Петя', 'salary'=>500],
+];
+
+foreach ($arr as $elem) {
+    echo $elem['name']. '-'. $elem['salary']. '<br>';
 }
-echo $str;
