a57d6615ba6765d6beef206a49c7062be812d22c
NegaTiv1298 <levkristi@i.ua>
2021-03-02 22:44:07 +0200

    Ex.21.17

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-$arr = [1, 3, -2, -5, 9, 11, 7];
-foreach ($arr as $elem) {
-    if ($elem > 0 && $elem < 10) {
-        $result += $elem;
+for ($i = 1; $i <= 9; $i++) {
+    for ($j = 1; $j <= $i; $j++) {
+        $str .= $i;
     }
 }
-echo $result;
+echo $str;
