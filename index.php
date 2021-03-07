f998796d80ac187f32f86a127e5776c2d21a7128
NegaTiv1298 <levkristi@i.ua>
2021-03-07 21:56:12 +0200

    Ex.22.35

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-$arr = ['http://google.com', 'yandex.ru', 'http://code.mu', 'youtube'];
-$result = [];
-foreach ($arr as $elem) {
-    $str = implode(',', $arr);
-    if (strpos($elem, 'http://') === 0) {
-        $result[] = $elem;
-    }
-}
-var_dump($result);
+$now = time();
+$nY = mktime(0,0,0,12,31);
+$result = $nY - $now;
+echo 'До нового года осталось : '. floor($result / (60*60*24));
