4932bba913f498a158ffad7a42203b9c598a4e5a
NegaTiv1298 <levkristi@i.ua>
2021-03-07 23:09:16 +0200

    Ex.22.2

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
+<form action="" method="get">
+    <input type="text" name="text" placeholder="Введите год">
+    <input type="submit">
+</form>
 <?php
-$now = time();
-$nY = mktime(0,0,0,12,31);
-$result = $nY - $now;
-echo 'До нового года осталось : '. floor($result / (60*60*24));
+if (isset($_REQUEST['text'])) {
+    $year = trim($_REQUEST['text']);
+    if (strlen($year) == 4) {
+        if (date('L', mktime(0,0,0,1,1,$year))) {
+            echo 'Высокосный';
+        } else {
+            echo 'Не высокосный';
+        }
+    }
+}
