d9efabab14c46277007f93ced91046b673fab3f1
NegaTiv1298 <levkristi@i.ua>
2021-03-07 16:18:15 +0200

    Ex.21.30

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-$arr = [1, 2, 3, 4, 5, 6, 7];
-$arr2 = [];
-$key = [];
-$num = count($arr);
-for ($i = 1; $i <= $num - 1; $i+=2) {
-    $key = $arr[$i];
-    $arr2[$key] = $arr[$i + 1];
-}
-var_dump($arr2);
+$str = '12345678';
+$result = array_reverse(str_split(strrev($str), 2));
+echo implode('', $result);
