bb82315e818d78308c842e32bc4e867d96922b60
NegaTiv1298 <levkristi@i.ua>
2021-03-09 22:25:29 +0200

    Ex.22.5

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if (isset($_REQUEST['text'])) {
     $year = trim($_REQUEST['text']);
     $arr = explode('.', $year);
     $mtime = mktime(0,0, 0, $arr[1], $arr[0], $arr[2]);
-    $week = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
-    $day = date('w', $mtime);
-    echo $week[$day];
+    $hb = $mtime - time();
+    $result = date('z', $hb);
+    echo $result;
 }
