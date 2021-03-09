faab7652a53483643e2622637a8696b5aad972ff
NegaTiv1298 <levkristi@i.ua>
2021-03-09 22:37:31 +0200

    Ex.22.10

--- index.php
+++ index.php
@@ -1,45 +1,13 @@
 <form action="" method="get">
-    <input type="text" name="text" placeholder="Введите месяц и день">
+    <textarea name="text"></textarea>
     <input type="submit">
 </form>
 <?php
 if (isset($_REQUEST['text'])) {
-    $date = explode('.', $_REQUEST['text']);
-    $dateStr = $date[1].$date[0];
-    if ($dateStr >= '0321' and $dateStr <= '0419') {
-        echo 'Овен';
-    }
-    if ($dateStr >= '0420' and $dateStr <= '0520') {
-        echo 'Телец';
-    }
-    if ($dateStr >= '0521' and $dateStr <= '0620') {
-        echo 'Близнецы';
-    }
-    if ($dateStr >= '0621' and $dateStr <= '0722') {
-        echo 'Рак';
-    }
-    if ($dateStr >= '0723' and $dateStr <= '0822') {
-        echo 'Лев';
-    }
-    if ($dateStr >= '0823' and $dateStr <= '0922') {
-        echo 'Дева';
-    }
-    if ($dateStr >= '0923' and $dateStr <= '1022') {
-        echo 'Весы';
-    }
-    if ($dateStr >= '1023' and $dateStr <= '1121') {
-        echo 'Скорпион';
-    }
-    if ($dateStr >= '1122' and $dateStr <= '1221') {
-        echo 'Стрелец';
-    }
-    if (($dateStr >= '1222' and $dateStr <= '1231') or ($dateStr >= '0101' and $dateStr <= '0119')) {
-        echo 'Козерог';
-    }
-    if ($dateStr >= '0120' and $dateStr <= '0218') {
-        echo 'Водолей';
-    }
-    if ($dateStr >= '0219' and $dateStr <= '0320') {
-        echo 'Рыбы';
-    }
+    $text = $_REQUEST['text'];
+    $strlen = strlen($text);
+    $count = count(explode(' ', $text));
+    $probel = $count - 1;
+    echo 'В тексте '. $count. ' слов '.$strlen. ' символов '. $probel. ' пробелов';
 }
+
