aa0f6d823795ca49c6aa65370dc39319aaf53b24
NegaTiv1298 <levkristi@i.ua>
2021-03-14 23:02:11 +0200

    Ex.24.6

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-$arr = ['a', 'b', 'c', 'd', 'e'];
-echo $arr[$_GET['get']];
+echo $_GET['get'];
+?>
+<br>
+<a href="?get=1">link 1</a>
+<a href="?get=2">link 2</a>
+<a href="?get=3">link 3</a>
+
