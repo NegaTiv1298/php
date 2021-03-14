d27b53ea1d3cc064e2e06641354c1b882dd3f515
NegaTiv1298 <levkristi@i.ua>
2021-03-14 22:46:59 +0200

    Ex.24.1

--- index.php
+++ index.php
@@ -1,21 +1,2 @@
 <?php
-$arr = [
-    ['name'=>'Коля', 'age'=>30, 'salary'=>500],
-    ['name'=>'Вася', 'age'=>31, 'salary'=>600],
-    ['name'=>'Петя', 'age'=>32, 'salary'=>700],
-];
-echo '<table>'. '<tr>';
-
-foreach ($arr as $elem) {
-    $name = $elem['name'];
-    $age = $elem['age'];
-    $salary = $elem['salary'];
-    echo
-    "<tr>         
-           <td>$name</td> 
-           <td>$age</td> 
-           <td>$salary</td>           
-    </tr>";
-}
-echo '</tr>'.'</table>';
-?>
+echo $_GET['get'];
