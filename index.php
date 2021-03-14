f3bb452b9087bd32060c495b10e8a1e2104c8e8d
NegaTiv1298 <levkristi@i.ua>
2021-03-14 21:51:54 +0200

    Ex.23.1

--- index.php
+++ index.php
@@ -1,30 +1,4 @@
 <?php
-$month = [1 => 'январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'];
-?>
-<form action="" method="get">
-    <select name="day">
-        <?php for ($i = 1; $i <= 31; $i++): ?>
-            <option><?=$i;?></option>
-        <?php endfor;?>
-    </select>
-    <select name="month">
-        <?php foreach ($month as $elem): ?>
-            <option><?=$elem;?></option>
-        <?php endforeach;?>
-    </select>
-    <select name="year">
-        <?php for ($i = 1990; $i <= 2025; $i++): ?>
-            <option><?=$i;?></option>
-        <?php endfor;?>
-    </select>
-    <input type="submit">
-</form>
-<?php
-$week = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
-if (isset($_REQUEST)) {
-    $day = $_REQUEST['day'];
-    $mon = array_search($_REQUEST['month'], $month);
-    $year = $_REQUEST['year'];
-    $dayOfWeek = $week[date('w', mktime(0,0,0,$mon, $day, $year))];
-    echo $dayOfWeek;
-}
+$name = 'Иван';
+echo 'Привет, ' . $name . '!';
+echo "Привет, $name!";
