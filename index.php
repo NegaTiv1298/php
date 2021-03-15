5961aaa60d468e11a93355fecc1f84306afe1f45
NegaTiv1298 <levkristi@i.ua>
2021-03-15 22:36:08 +0200

    Ex.24.9

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 echo $_GET['get'];
 echo '<br>';
 for ($i = 1; $i <= 10; $i++) {
-    echo "<p><a href=\"?get=$i\">link $i</a></p> ";
+    echo "<ul><li><a href=\"?get=$i\">link $i</a></li></ul> ";
 }
 ?>
