d9af7ae9c1cbeab6b5dec66a20eeafea02b8c8bf
NegaTiv1298 <levkristi@i.ua>
2021-03-15 23:07:44 +0200

    Ex.25.1

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
 <?php
-$arr = ['a', 'b', 'c', 'd', 'e'];
-if (isset($_GET['get']) and isset($arr[$_GET['get']])) {
-    echo $arr[$_GET['get']];
-    echo '<br>';
-}
-foreach ($arr as $key => $elem) {
-    echo "<a href=\"?get=$key\">link $elem</a> ";
-}
-?>
+$str = 'ahb acb aeb aeeb adcb axeb';
+echo preg_replace('#a.b#', '!', $str);
