d9b0b0aae226392ca35b42c238cd596f49b172e0
NegaTiv1298 <levkristi@i.ua>
2021-03-15 22:46:47 +0200

    Ex.24.10

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
-echo $_GET['get'];
-echo '<br>';
-for ($i = 1; $i <= 10; $i++) {
-    echo "<ul><li><a href=\"?get=$i\">link $i</a></li></ul> ";
+$arr = ['a', 'b', 'c', 'd', 'e'];
+if (isset($_GET['get']) and isset($arr[$_GET['get']])) {
+    echo $arr[$_GET['get']];
+    echo '<br>';
 }
 ?>
+<a href="?get=0">link 0</a>
+<a href="?get=1">link 1</a>
+<a href="?get=2">link 2</a>
+<a href="?get=3">link 3</a>
+<a href="?get=4">link 4</a>
+
