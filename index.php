919eedd5079f9449907460fe97974229afe8b4e6
NegaTiv1298 <levkristi@i.ua>
2021-03-15 22:50:00 +0200

    Ex.24.11

--- index.php
+++ index.php
@@ -4,10 +4,7 @@ if (isset($_GET['get']) and isset($arr[$_GET['get']])) {
     echo $arr[$_GET['get']];
     echo '<br>';
 }
+foreach ($arr as $key => $elem) {
+    echo "<a href=\"?get=$key\">link $elem</a> ";
+}
 ?>
-<a href="?get=0">link 0</a>
-<a href="?get=1">link 1</a>
-<a href="?get=2">link 2</a>
-<a href="?get=3">link 3</a>
-<a href="?get=4">link 4</a>
-
