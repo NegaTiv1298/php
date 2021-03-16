0c9afc757a911d6ecc3e3e137306ce69800e0e26
NegaTiv1298 <levkristi@i.ua>
2021-03-16 23:27:14 +0200

    Ex.27.6

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-$str = 'a1a a22a a333a a4444a a55555a aba aca';
-echo preg_replace('#a\d+a#', '!', $str);
+$str = 'aa a1a a22a a333a a4444a a55555a aba aca';
+echo preg_replace('#a\d*a#', '!', $str);
