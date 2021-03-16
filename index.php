349efc3a791379f436200d346c81f10156d3ccde
NegaTiv1298 <levkristi@i.ua>
2021-03-16 23:33:52 +0200

    Ex.27.12

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-$str = 'aba aea aca aza axa a.a a+a a*a';
-echo preg_replace('#a[b.*+]a#', '!', $str);
+//$str = 'aba aea aca aza axa a.a a+a a*a';
+echo preg_replace('#a[3-7]a#', '!', $str);
