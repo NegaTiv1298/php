bf21c50753a9ece70851e56fb4e968ad6bedc177
NegaTiv1298 <levkristi@i.ua>
2021-03-17 22:02:17 +0200

    Ex.28.7

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
 <?php
-$str = 'mail@yandex.ru mail@yandex.ru mail mail@yandex.ru yandex';
-echo preg_match_all('#[a-z0-9_.-]+@[a-z_.-]+\.[a-z]{2,3}#', $str, $m). '<br>';
-var_dump($m);
+$str = 'yandex12-3.ru';
+echo preg_match('#[a-z0-9-]\.[a-z]{2,3}#', $str);
