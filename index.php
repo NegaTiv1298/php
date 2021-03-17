f6619384514cd9bdccc6759e25637fada7da02ca
NegaTiv1298 <levkristi@i.ua>
2021-03-17 22:15:37 +0200

    Ex.28.11

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-$str = 'http://site.ru';
-echo preg_match('#^http?://[a-z0-9-]+\.[a-z]{2,3}$#', $str);
+$str = 'https://site.ru/';
+echo preg_match('#^https?://[a-z0-9-]+\.[a-z]{2,3}/?$#', $str);
