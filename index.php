aebd1b98d78b2be9128863c877ad5652a8deeaa3
NegaTiv1298 <levkristi@i.ua>
2021-03-17 21:58:47 +0200

    Ex.28.6

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-$str = 'mail@yandex.ru';
-echo preg_match('#^[a-z0-9_.-]+@[a-z_.-]+\.[a-z]{2,3}$#', $str);
+$str = 'mail@yandex.ru mail@yandex.ru mail mail@yandex.ru yandex';
+echo preg_match_all('#[a-z0-9_.-]+@[a-z_.-]+\.[a-z]{2,3}#', $str, $m). '<br>';
+var_dump($m);
