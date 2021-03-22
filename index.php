68cb94a3d172ddb268814909211cf3e1e7ed1121
NegaTiv1298 <levkristi@i.ua>
2021-03-22 23:26:30 +0200

    Ex.31.5

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
-file_put_contents('new.txt', '');
-echo file_get_contents('new.txt');
+$arr = ['1.txt', '2.txt', '3.txt'];
+foreach ($arr as $elem) {
+    file_put_contents($elem, '');
+    echo file_get_contents($elem);
+}
