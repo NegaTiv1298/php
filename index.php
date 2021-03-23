960ab490398e08c88fee168f25ca9e26705d75c0
NegaTiv1298 <levkristi@i.ua>
2021-03-23 23:28:36 +0200

    Ex.31.20

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
-if (file_exists('test.php') == true ){
-    unlink('test.php');
-} else {
-    file_put_contents('test.php', '');
+$arr = ['1.txt', '2.txt', '3.txt'];
+foreach ($arr as $elem) {
+    file_put_contents($elem, '2');
+    if (file_exists($elem) == true) {
+        echo $elem. ' - '. 'Существует'. '<br>';
+    } else {
+        echo $elem. ' - '. 'Не существует'. '<br>';
+    }
 }
