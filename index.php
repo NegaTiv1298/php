bb08468c5a70dd465a77e153d8dd83168d2f5e49
NegaTiv1298 <levkristi@i.ua>
2021-03-23 22:30:57 +0200

    Ex.31.6

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
 <?php
-$arr = ['1.txt', '2.txt', '3.txt'];
-foreach ($arr as $elem) {
-    file_put_contents($elem, '');
-    echo file_get_contents($elem);
-}
+$text = file_get_contents('text.txt');
+file_put_contents('text.txt', $text. '!');
