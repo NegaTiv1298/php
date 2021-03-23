f5a11271b6b3e8a6533dd107046075aeed5ea9eb
NegaTiv1298 <levkristi@i.ua>
2021-03-23 23:14:39 +0200

    Ex.31.10

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 $arr = ['1.txt', '2.txt', '3.txt'];
 foreach ($arr as $elem) {
-    file_put_contents($elem, '2');
-    $get[] = file_get_contents($elem);
-    $impl = implode('', $get);
-    file_put_contents('new.txt', $impl);
+    $get = file_get_contents($elem);
+    file_put_contents($elem, $get. '!');
 }
-echo file_get_contents('new.txt');
