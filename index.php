66b26d5ae342b895084b171431b4e28e423612a6
NegaTiv1298 <levkristi@i.ua>
2021-03-23 22:35:37 +0200

    Ex.31.7

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-$text = file_get_contents('text.txt');
-file_put_contents('text.txt', $text. '!');
+$text = pow(file_get_contents('text.txt'), 2);
+file_put_contents('text.txt', $text);
+echo $text;
