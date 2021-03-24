a496f48ffcce02c5dd92dba01481b270fdf69394
NegaTiv1298 <levkristi@i.ua>
2021-03-24 23:14:00 +0200

    Ex.31.32

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-$arr = ['dir1', 'dir2'];
-foreach ($arr as $elem) {
-    mkdir($elem);
-}
+mkdir('test');
+file_put_contents('1.txt', '1');
+file_put_contents('2.txt', '1');
+file_put_contents('3.txt', '1');
+rename('1.txt', 'test/1.txt');
+rename('2.txt', 'test/2.txt');
+rename('3.txt', 'test/3.txt');
