d4a52c51ca5de4a2a53e69fee42a63ccea544ee0
NegaTiv1298 <levkristi@i.ua>
2021-03-24 22:17:32 +0200

    Ex.31.25

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-$arr = ['1.txt', '2.txt', '3.txt'];
-foreach ($arr as $elem) {
-    file_put_contents($elem, '2');
-    if (file_exists($elem) == true) {
-        echo $elem. ' - '. 'Существует'. '<br>';
-    } else {
-        echo $elem. ' - '. 'Не существует'. '<br>';
-    }
-}
+$text = file_get_contents('text.txt');
+$arr = explode("\r\n", $text);
+var_dump($arr);
