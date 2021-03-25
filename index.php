5a8422aaa545cf1fcf78deb86bc3b982a3182b4c
NegaTiv1298 <levkristi@i.ua>
2021-03-25 23:16:56 +0200

    Ex.31.37(2)

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 $arr = array_diff(scandir('dir'), ['..', '.']);
 foreach ($arr as $elem) {
-    $text = file_get_contents("dir/$elem");
-    file_put_contents("dir/$elem", $text. '!');
+    echo file_get_contents('dir/'. $elem);
 }
