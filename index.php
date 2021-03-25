dda01db496b0926d523ea3254bd4c12949b389fe
NegaTiv1298 <levkristi@i.ua>
2021-03-25 22:41:35 +0200

    Ex.31.35

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
-mkdir('test');
-file_put_contents('1.txt', '1');
-file_put_contents('2.txt', '1');
-file_put_contents('3.txt', '1');
-rename('1.txt', 'test/1.txt');
-rename('2.txt', 'test/2.txt');
-rename('3.txt', 'test/3.txt');
+$files = scandir('test');
+var_dump($files);
