6c6c55de8eab2dde1260e5357abcd739347aa6b6
NegaTiv1298 <levkristi@i.ua>
2021-03-25 23:45:18 +0200

    Ex.31.42

--- index.php
+++ index.php
@@ -2,13 +2,21 @@
 function getFiles($dir)
 {
     $arr = array_diff(scandir('dir'), ['..', '.']);
+    $result = [];
     foreach ($arr as $elem) {
         $path = $dir . '/' . $elem;
         if (is_dir($path)) {
-            getFiles($path);
+            $result = array_merge($result, getFiles($path));
         } else {
-            var_dump($path);
+            $result[] = $path;
         }
     }
+    return $result;
 }
-getFiles('dir');
+$arr = getFiles('dir');
+foreach ($arr as $elem) {
+    if (is_file($elem)) {
+        $text = file_get_contents($elem);
+        file_put_contents($elem, $text. '!');
+    }
+}
