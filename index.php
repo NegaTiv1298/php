61e661d0f8ea55e4e21df1c944c7bcebfaf905bf
NegaTiv1298 <levkristi@i.ua>
2021-03-25 23:36:25 +0200

    Ex.31.41

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
-$arr = array_diff(scandir('dir'), ['..', '.']);
-foreach ($arr as $elem) {
-    echo $elem. '-';
-    var_dump(is_file('dir/'. $elem));
+function getFiles($dir)
+{
+    $arr = array_diff(scandir('dir'), ['..', '.']);
+    foreach ($arr as $elem) {
+        $path = $dir . '/' . $elem;
+        if (is_dir($path)) {
+            getFiles($path);
+        } else {
+            var_dump($path);
+        }
+    }
 }
-
+getFiles('dir');
