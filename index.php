890eb7e60e25bdf5324746286c46cec777ce8e72
NegaTiv1298 <levkristi@i.ua>
2021-03-25 23:21:00 +0200

    Ex.31.38(2)

--- index.php
+++ index.php
@@ -1,7 +1,2 @@
 <?php
-$arr = array_diff(scandir('dir'), ['..', '.']);
-foreach ($arr as $elem) {
-    echo $elem;
-    var_dump(is_file('dir/'. $elem));
-    var_dump(is_dir('dir/'. $elem));
-}
+var_dump(is_file('text.txt'));
