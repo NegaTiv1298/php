a3bc6b0b8b766ab3f2d9d5cd61454f58ab1fdf4e
NegaTiv1298 <levkristi@i.ua>
2021-03-28 22:38:41 +0300

    Ex.32.1

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
 <?php
-function getFiles($dir)
-{
-    $arr = array_diff(scandir($dir), ['..', '.']);
-    $result = [];
-    foreach ($arr as $elem) {
-        $path = $dir . '/' . $elem;
-        if (is_dir($path)) {
-            $result = array_merge($result, getFiles($path));
-        } else {
-            $result[] = $path;
-        }
-    }
-    return $result;
-}
-$arr = getFiles('dir');
-foreach ($arr as $elem) {
-    if (is_file($elem)) {
-        $text = file_get_contents($elem);
-        file_put_contents($elem, $text. '!');
-    }
-}
+session_start();
+$_SESSION['test'] = 'Test';
+echo $_SESSION['test'];
