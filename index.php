7e34c944f3c035ba6b971308c2d93eb641e623bc
NegaTiv1298 <levkristi@i.ua>
2021-03-28 23:27:46 +0300

    Ex.32.6

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 session_start();
-if (empty($_SESSION['time'])) {
-    $_SESSION['time'] = time();
+if (isset($_REQUEST['email'])) {
+    $_SESSION['email'] = $_REQUEST['email'];
 }
-echo time() - $_SESSION['time'];
+?>
+<form action="" method="get">
+    <input type="text" name="email">
+    <input type="submit">
+</form>
+
+<form action="" method="get">
+    <input type="text" name="name">
+    <input type="text" name="surname">
+    <input type="text" name="email" value="<?php if (isset($_SESSION['email'])) echo $_SESSION['email']; ?>">
+    <input type="submit">
+</form>
