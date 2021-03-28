277e441105765a21f49fd4966bf496c6116a1f1b
NegaTiv1298 <levkristi@i.ua>
2021-03-28 23:33:03 +0300

    Ex.33.1

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
 <?php
-session_start();
-if (isset($_REQUEST['email'])) {
-    $_SESSION['email'] = $_REQUEST['email'];
-}
-?>
-<form action="" method="get">
-    <input type="text" name="email">
-    <input type="submit">
-</form>
-
-<form action="" method="get">
-    <input type="text" name="name">
-    <input type="text" name="surname">
-    <input type="text" name="email" value="<?php if (isset($_SESSION['email'])) echo $_SESSION['email']; ?>">
-    <input type="submit">
-</form>
+setcookie("test", "123123", time() + 3600);
+echo $_COOKIE['test'];
