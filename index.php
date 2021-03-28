e1cf664245952a4c703f611be6e2371ae97b5b3b
NegaTiv1298 <levkristi@i.ua>
2021-03-28 23:05:30 +0300

    Ex.32.4

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 session_start();
-$_SESSION['123'] = 'Test123';
+if (!empty($_REQUEST['country'])) {
+    $_SESSION['country'] = $_REQUEST['country'];
+} else {
+    $_SESSION['country'] = 'Введите вашу страну';
+}
+?>
+<form action="" method="get">
+    <input type="text" name="country">
+    <input type="submit" name="sumbit">
+</form>
--- test.php
+++ test.php
@@ -1,8 +1,3 @@
 <?php
 session_start();
-if (!isset($_SESSION['counter'])) {
-    $_SESSION['counter'] = 1;
-} else {
-    $_SESSION['counter'] = $_SESSION['counter'] + 1;
-}
-echo 'Вы обновили страницу '. $_SESSION['counter']. ' раз!';
+echo $_SESSION['country'];
