f85c7a3e23dec32dd5d04e3ce7dc5652b9ef98dc
NegaTiv1298 <levkristi@i.ua>
2021-03-29 22:50:46 +0300

    Ex.33.3

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
-setcookie("test", "123123", time());
-echo $_COOKIE['test'];
+if (!isset($_COOKIE['test'])) {
+    setcookie('test', '1', time() + 3600);
+} else {
+    setcookie('test', ($_COOKIE['test'] + 1), time() + 3600);
+}
+echo 'Вы посетили страницу '. $_COOKIE['test']. ' раз!';
