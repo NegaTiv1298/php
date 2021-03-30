eeb549e2260b5bfe21d5c252b6479653004ea2c9
NegaTiv1298 <levkristi@i.ua>
2021-03-30 23:32:04 +0300

    Ex.33.4(2)

--- index.php
+++ index.php
@@ -1,28 +1,19 @@
-<form action="" method="get">
-    <p><b>Введите дату своего рождения</b></p><input type="text" name="birthday">
-    <input type="submit">
-</form>
 <?php
 if (!empty($_REQUEST['birthday'])) {
     setcookie('birthday', $_REQUEST['birthday'], time() + 3600);
-}
-if (isset($_REQUEST['birthday']) && !empty($_REQUEST['birthday'])) {
-        $arr = explode('-', $_REQUEST['birthday']);
-        $day = $arr[2] + 1;
-        $month = $arr[1];
-        $year = date('Y');
-        $birthForm = "$year-$month-$day";
-        if (strtotime($birthForm) < time()) {
-            $year = date('Y') + 1;
-            $birthForm = "$year-$month-$day";
-        }
-        $checkTime = strtotime($birthForm) - time();
+        $dateR = $_REQUEST['birthday'];
+        $chekTime = strtotime($dateR) - time();
         if ($_COOKIE['birthday']) {
-            $birthday = floor($checkTime / (60 * 60 * 24));
+            $birthday = floor($chekTime / (60*60*24));
             if ($birthday == 0) {
-                echo 'Поздравляем з днем рождения!!!';
+                echo 'Поздравляем з днем рождения!!';
             } else {
-                echo 'Сегодня ' . date('d.m.Y', time()) . '<br>' . 'До вашего дня рождения осталось -' . $birthday . 'дней.';
+                echo 'Сегодня ' . date('d.m.Y', time()) . '<br>' . 'До Вашего дня рождения осталось ' . $birthday . ' дней';
             }
         }
 }
+?>
+<form action="" method="get">
+    <p><b>Введите дату своего рождения</b></p><input type="date" name="birthday">
+    <input type="submit">
+</form>
