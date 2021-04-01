c354c94a5d0cfc5650cba272b10b32f2a1fbfdb3
NegaTiv1298 <levkristi@i.ua>
2021-04-01 23:26:30 +0300

    Ex.35.19

--- index.php
+++ index.php
@@ -8,7 +8,9 @@ $link = mysqli_connect($host, $user, $password, $db_name);
 
 mysqli_query($link, "SET NAMES 'utf8'");
 
-$query = "SELECT * FROM workers WHERE salary=1000 OR age>=23 AND age<27";
+//$query = "SELECT * FROM workers WHERE salary>=400 AND salary<=1000 OR age>=23 AND age<=27";
+$query = "INSERT INTO workers (name, age, salary)
+     VALUES ('Ярослав', 30, 1200), ('Петр', 31, 1000)";
 
 $result = mysqli_query($link, $query) or die(mysqli_error($link));
 
