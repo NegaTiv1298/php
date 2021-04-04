d2348e4a2a7bf7213ef6b3c5cf436790b1919bb7
NegaTiv1298 <levkristi@i.ua>
2021-04-04 15:54:48 +0300

    Ex.36.4

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $link = mysqli_connect($host, $user, $password, $db_name);
 
 mysqli_query($link, "SET NAMES 'utf8'");
 
-$query = "SELECT * FROM workers WHERE id>0 ORDER BY salary";
+$query = "SELECT * FROM workers WHERE id>0 ORDER BY salary DESC";
 
 $result = mysqli_query($link, $query) or die(mysqli_error($link));
 
