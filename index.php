77453934a63df366e50101366bc92013fca27192
NegaTiv1298 <levkristi@i.ua>
2021-04-04 22:02:52 +0300

    Ex.38

--- index.php
+++ index.php
@@ -14,9 +14,16 @@ mysqli_query($link, "SET NAMES 'utf8'");
 		<th>name</th>
 		<th>age</th>
 		<th>salary</th>
+        <th>delete</th>
 	</tr>
 
 <?php
+if (isset($_GET['del'])) {
+    $del = $_GET['del'];
+    $query = "DELETE FROM workers WHERE id=$del";
+    mysqli_query($link, $query) or die(mysqli_error($link));
+}
+
 $query = "SELECT * FROM workers";
 $result = mysqli_query($link, $query) or die(mysqli_error($link));
 
@@ -33,6 +40,7 @@ foreach ($data as $elem) {
     $result .= '<td>'. $elem['name']. '</td>';
     $result .= '<td>'. $elem['age']. '</td>';
     $result .= '<td>'. $elem['salary']. '</td>';
+    $result .= '<td><a href="?del=' .$elem['id']. '">удалить</a></td>';
 
     $result .= '</tr>';
 }
