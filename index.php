499c61a6ed55dade6c6dc2976a6fb53854c43058
NegaTiv1298 <levkristi@i.ua>
2021-04-07 22:04:58 +0300

    OOP.Less.4.(Ex.4.7-Ex.4.10)

--- index.php
+++ index.php
@@ -1,42 +1,19 @@
 <?php
-class Employee
+class User
 {
     public $name;
     public $age;
-    public $salary;
 
-    public function getName()
+    public function setAge($age)
     {
-        return $this->name;
-    }
-
-    public function getAge()
-    {
-        return $this->age;
-    }
-
-    public function getSalary()
-    {
-        return $this->salary;
-    }
-    public function checkAge()
-    {
-        if ($this->age > 18) {
-            return true;
-        } else {
-            return false;
+        if ($age > 18) {
+            $this->age = $age;
         }
     }
 }
+$user = new User;
+$user->age = 24;
+$user->name = 'Коля';
+$user->setAge(19);
 
-$user1 = new Employee;
-$user1->name = 'Вася';
-$user1->age = 24;
-$user1->salary = 800;
-
-$user2 = new Employee;
-$user2->name = 'Дима';
-$user2->age = 19;
-$user2->salary = 500;
-
-echo $user1->getSalary() + $user2->getSalary();
+echo $user->age;
