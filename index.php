e46f2223027780952aa9a4be6f5b033ff17e0aa5
NegaTiv1298 <levkristi@i.ua>
2021-04-07 21:55:32 +0300

    OOP.Less.4.(Ex.4.1-Ex.4.6)

--- index.php
+++ index.php
@@ -4,13 +4,30 @@ class Employee
     public $name;
     public $age;
     public $salary;
-    public function show ($str)
+
+    public function getName()
     {
-        return $str . ' ' . 'Hello';
+        return $this->name;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function getSalary()
+    {
+        return $this->salary;
+    }
+    public function checkAge()
+    {
+        if ($this->age > 18) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
-
-
 
 $user1 = new Employee;
 $user1->name = 'Вася';
@@ -19,7 +36,7 @@ $user1->salary = 800;
 
 $user2 = new Employee;
 $user2->name = 'Дима';
-$user2->age = 27;
-$user2->salary = 2300;
+$user2->age = 19;
+$user2->salary = 500;
 
-echo $user2->show('Dima');
+echo $user1->getSalary() + $user2->getSalary();
