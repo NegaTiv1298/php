d630c5e49b534563a7bcdf510cbf86128957c5b5
NegaTiv1298 <levkristi@i.ua>
2021-04-07 21:44:51 +0300

    OOP.Less.3

--- index.php
+++ index.php
@@ -1,22 +1,25 @@
 <?php
-//1
 class Employee
 {
     public $name;
     public $age;
     public $salary;
+    public function show ($str)
+    {
+        return $str . ' ' . 'Hello';
+    }
 }
-//2
+
+
+
 $user1 = new Employee;
 $user1->name = 'Вася';
 $user1->age = 24;
 $user1->salary = 800;
-//3
+
 $user2 = new Employee;
 $user2->name = 'Дима';
 $user2->age = 27;
 $user2->salary = 2300;
-//4
-echo $user1->salary + $user2->salary.'<br>';
-//5
-echo $user1->age + $user2->age;
+
+echo $user2->show('Dima');
