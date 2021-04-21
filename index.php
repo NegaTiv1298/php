fb738074fad2220a9de17cd642d88866f78b2691
NegaTiv1298 <levkristi@i.ua>
2021-04-21 23:25:09 +0300

    OOP.Less.33(Ex.33.1)

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
-require_once 'Circle.php';
+require_once 'Test1.php';
 require_once 'Employee.php';
 require_once 'Post.php';
-require_once 'Geometry.php';
+require_once 'Test2.php';
 require_once 'UsersCollection.php';
 
 $circle = new Circle(10);
-echo $circle->getCircuit(10).'<br>';
-echo $circle->getSquare(10);
+echo get_class($circle);
