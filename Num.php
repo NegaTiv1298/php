37498ab622d1d3bf879611d109ff0e7edd6a91cb
NegaTiv1298 <levkristi@i.ua>
2021-04-21 15:31:29 +0300

    OOP.Less.30(Ex.30.2,3)

--- Num.php
+++ Num.php
@@ -1,6 +1,11 @@
 <?php
 class Num
 {
-    public static $num1;
-    public static $num2;
+    private static $num1 = 2;
+    private static $num2 = 3;
+
+    public function getSum()
+    {
+        return self::$num1 + self::$num2;
+    }
 }
--- index.php
+++ index.php
@@ -6,6 +6,4 @@ require_once 'Num.php';
 require_once 'UsersCollection.php';
 
 
-Num::$num1 = 1;
-Num::$num2 = 2;
-echo Num::$num1 + Num::$num2;
+echo Num::getSum();
