d65a5888e8162fe201f54b39146035712b4f3e0f
NegaTiv1298 <levkristi@i.ua>
2021-04-21 13:35:43 +0300

    OOP.Less.27(Ex.27.6)

--- StudenT.php
+++ StudenT.php
@@ -1,20 +1,12 @@
 <?php
 class StudenT
 {
-    private $name;
-    private $scholarship;
+    public $name;
+    public $scholarship;
 
     public function __construct($name, $scholarship)
     {
         $this->name = $name;
         $this->scholarship = $scholarship;
-    }
-    public function getName()
-    {
-        return $this->name;
-    }
-    public function getScholarship()
-    {
-        return $this->scholarship;
     }
 }
--- index.php
+++ index.php
@@ -5,19 +5,42 @@ require_once 'StudenT.php';
 require_once 'Employee2.php';
 require_once 'UsersCollection.php';
 
-$usersCollection = new UsersCollection;
+$arr = [
+    new Employee('Коля', 730),
+    new Employee('Дима', 320),
+    new Employee('Вася', 400),
 
-$usersCollection->add(new Student('Петя', 100));
-$usersCollection->add(new Student('Ваня', 200));
-
-$usersCollection->add(new Employee('Коля', 300));
-$usersCollection->add(new Employee('Вася', 400));
-
-
-echo $usersCollection->getTotalScholarship().'<br>';
-
-
-echo $usersCollection->getTotalSalary().'<br>';
+    new StudenT('Витя', 180),
+    new StudenT('Петя', 230),
+    new StudenT('Едик', 215)
+];
+var_dump($arr);
 
 
-echo $usersCollection->getTotalPayment();
+foreach ($arr as $elem) {
+    if ($elem instanceof Employee) {
+        echo $elem->name. '<br>';
+    }
+}
+echo '<br>';
+foreach ($arr as $elem) {
+    if ($elem instanceof StudenT) {
+        echo $elem->name. '<br>';
+    }
+}
+echo '<br>';
+$arrSalary = [];
+foreach ($arr as $elem) {
+    if ($elem instanceof Employee) {
+        $arrSalary[] = $elem->salary;
+    }
+}
+echo array_sum($arrSalary).'<br>';
+
+$arrScholarship = [];
+foreach ($arr as $elem) {
+    if ($elem instanceof StudenT) {
+        $arrScholarship[] = $elem->scholarship;
+    }
+}
+echo array_sum($arrScholarship);
