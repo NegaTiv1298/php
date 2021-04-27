b704f61240ddc4566be2d8577354baf4e6737f01
NegaTiv1298 <levkristi@i.ua>
2021-04-27 23:27:48 +0300

    OOP.Less.47

--- Test.php
+++ Test.php
@@ -1,9 +1,13 @@
 <?php
 class Test
 {
-    use Trait1, Trait2, Trait3;
+    use Trait1, Trait2, Trait3 {
+        Trait2::met insteadof Trait3;
+        Trait2::met as method2;
+        Trait3::met as method3;
+    }
     public function getSum()
     {
-        return $this->method1() + $this->method2() + $this->method3();
+        return $this->method() + $this->method2() + $this->method3();
     }
 }
