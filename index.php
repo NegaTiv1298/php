5d65a105b24b8972e24129e334ef82fd75d2448d
NegaTiv1298 <levkristi@i.ua>
2021-05-12 23:14:09 +0300

    OOP.Less.91(Ex.1) Test spl_autoload

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
-spl_autoload_register();
+//spl_autoload_register();
+//
+//$data = new \Project\User\Data();
+//$user = new \Core\User();
+//$controller = new \Core\Admin\Controller();
+//
+//echo $data->ret();
+//echo $user->get();
+//echo $controller->get();
+
+spl_autoload_register(function($class) {
+    $ds = DIRECTORY_SEPARATOR;
+    $filename = $_SERVER['DOCUMENT_ROOT'] . $ds . str_replace('\\', $ds, $class) . '.php';
+    require($filename);
+});
 
 $data = new \Project\User\Data();
-$user = new \Core\User();
-$controller = new \Core\Admin\Controller();
-
 echo $data->ret();
-echo $user->get();
-echo $controller->get();
-
